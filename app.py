68e3f10bf63f117f7a96ddb176420fa24474a367
Pritam Kundu <pritamkundu3745@gmail.com>
2025-05-26 22:07:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,38 +1,62 @@
 import streamlit as st
 import pickle
-import pandas as pd
+import requests
 
-# Load movie data and similarity matrix
-movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
+api_key = "7cd2b0b1222b30ddc1b124bd131e5ca1"
+
+movies = pickle.load(open('movies.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-# Function to recommend movies
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        poster_path = data.get('poster_path')
+        return f"https://image.tmdb.org/t/p/w500/{poster_path}"
+
+    except Exception as e:
+        return None
+
+
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies = []
+    rec_movies = []
+    rec_posters = []
+    rec_links = []
+
     for i in movies_list:
-        title = movies.iloc[i[0]].title
         movie_id = movies.iloc[i[0]].movie_id
+        title = movies.iloc[i[0]].title
+        poster = fetch_poster(movie_id)
         link = f"https://www.themoviedb.org/movie/{movie_id}"
-        recommended_movies.append((title, link))
 
-    return recommended_movies
+        rec_movies.append(title)
+        rec_posters.append(poster)
+        rec_links.append(link)
 
-# Streamlit UI
+    return rec_movies, rec_posters, rec_links
+
 st.title('🎬 Movie Recommender System')
 
 selected_movie = st.selectbox(
     'Search for a movie you like:',
-    movies['title'].values
-)
+    movies['title'].values)
 
 if st.button('Recommend'):
-    recommendations = recommend(selected_movie)
-    st.subheader("Recommended Movies:")
+    titles, posters, links = recommend(selected_movie)
 
-    for title, link in recommendations:
-        st.markdown(f"[{title}]({link})", unsafe_allow_html=True)
+    st.subheader("Recommended Movies:")
+    cols = st.columns(5)
+
+    for i in range(5):
+        with cols[i]:
+            if posters[i]:
+                st.image(posters[i], use_container_width=True)
+            else:
+                st.image("https://via.placeholder.com/300x450?text=No+Image", use_container_width=True)
+            st.markdown(f"[{titles[i]}]({links[i]})", unsafe_allow_html=True)
